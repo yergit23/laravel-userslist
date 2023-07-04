5f0a4255eb9595b8c2a3b8e74def2802d22ce566
yergit23 <yerzhan23kes@gmail.com>
2023-07-04 19:09:37 +0600

    first change frontend

--- resources/views/page_register.blade.php
+++ resources/views/page_register.blade.php
@@ -32,7 +32,7 @@
                         <div class="page-logo width-mobile-auto m-0 align-items-center justify-content-center p-0 bg-transparent bg-img-none shadow-0 height-9 border-0">
                             <a href="javascript:void(0)" class="page-logo-link press-scale-down d-flex align-items-center">
                                 <img src="img/logo.png" alt="SmartAdmin WebApp" aria-roledescription="logo">
-                                <span class="page-logo-text mr-1">Учебный проект</span>
+                                <span class="page-logo-text mr-1">Список пользователей</span>
                             </a>
                         </div>
                         <span class="text-white opacity-50 ml-auto mr-2 hidden-sm-down">
